7bbb9c79f21ea405adacd21d576ee3dda72039ef
JuancaArg <carlosargueda31@gmail.com>
2023-02-12 01:18:07 -0500

    archivos cargados

---
+++ Mdy_Sig_Web/resources/views/Reutilizables/Head.blade.php
@@ -0,0 +1,16 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>SIG</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="src/vendors/typicons/typicons.css">
+  <link rel="stylesheet" href="src/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="src/css/vertical-layout-light/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="src/images/Logo-web.jpg" />
+</head>
---
+++ Mdy_Sig_Web/resources/views/Reutilizables/Scripts.blade.php
@@ -0,0 +1,14 @@
+  <!-- base:js -->
+  <script src="src/vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="src/js/off-canvas.js"></script>
+  <script src="src/js/hoverable-collapse.js"></script>
+  <script src="src/js/template.js"></script>
+  <script src="src/js/settings.js"></script>
+  <script src="src/js/todolist.js"></script>
+  <!-- endinject -->
+
+
+  <script src="Controlador/Controlador_ajax.js?"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
