6a139c0e1aa1f1c70b5a5980772f353d8a4048ee
DBaldera <d.baldera.17@gmail.com>
2023-03-07 16:56:54 -0500

    up20230307v3

--- Mdy_Sig_Web/public/js/Propios/Controlador_ajax_Duver.js
+++ Mdy_Sig_Web/public/js/Propios/Controlador_ajax_Duver.js
@@ -1,6 +1,8 @@
 
 var ruta = "http://localhost/mdy_sig/mdy_sig_web/public/";
 
+//Buscar Datos
+
 $('#Contenido_VA_Btn_Buscar_Documento').click(function () {
 
     //Extrae informacion
@@ -62,17 +64,40 @@ $('#Contenido_VA_Btn_Buscar_Documento').click(function () {
 
 
 var siglas = ['A', 'FI']
-var motivo = ['FALLO EN EL PROCESO DE ALTA DE PERSONAL', 'FALLO DE OMISION DE PERSONAL', 'FALLO DE REGISTRO EN SISTEMA', 'RETRASO DEL PROCESO DE CRECIMIENTO', 'FALTA DE ACTUALIZACIÓN DEL CARGO FUNCIONAL', 'FALLO EN PC, MARCACIÓN TARDÍA (ACTUALIZACIÓN DE TARDANZA)']
+var motivo_A = ['FALLO EN EL PROCESO DE ALTA DE PERSONAL', 'FALLO DE OMISION DE PERSONAL', 'FALLO DE REGISTRO EN SISTEMA', 'RETRASO DEL PROCESO DE CRECIMIENTO', 'FALTA DE ACTUALIZACIÓN DEL CARGO FUNCIONAL', 'FALLO EN PC, MARCACIÓN TARDÍA (ACTUALIZACIÓN DE TARDANZA)']
+var motivo_FI = ['ERROR DE MARCACIÓN']
 
 for (let index = 0; index < siglas.length; index++) {
     opciones = '<option>' + siglas[index] + '</option>';
     $('#Contenido_VA_Lista_Sigla').append(opciones);
 };
 
-for (let index = 0; index < motivo.length; index++) {
-    opciones_1 = '<option>' + motivo[index] + '</option>';
-    $('#Contenido_VA_Lista_Motivo').append(opciones_1);
-};
+// Desactivar el cambpo del costado si se selecciona DS
+
+$('#Contenido_VA_Lista_Sigla').on('change', function () {
+
+    var opciones_motivo = null
+    $('#Contenido_VA_Lista_Motivo').empty();
+
+    if ($(this).val() == 'A') {
+        
+        for (let index = 0; index < motivo_A.length; index++) {
+            opciones_motivo = '<option>' + motivo_A[index] + '</option>';
+            $('#Contenido_VA_Lista_Motivo').append(opciones_motivo);
+        };
+
+    }else{
+
+        for (let index = 0; index < motivo_FI.length; index++) {
+            opciones_motivo = '<option>' + motivo_FI[index] + '</option>';
+            $('#Contenido_VA_Lista_Motivo').append(opciones_motivo);
+        };
+    }
+
+})
+
+
+//Guardar
 
 $('#btn-sol-val-asistencia').click(function () {
 
@@ -82,43 +107,50 @@ $('#btn-sol-val-asistencia').click(function () {
     let DatoCompleto = 1;
 
     if (databla['Contenido_VA_Documento'] == ''
-        && databla['Contenido_VA_Lista_Sigla'] == 'Seleccionar sigla'
-        && databla['Contenido_VA_Lista_Motivo'] == 'Seleccionar motivo') {
+        || databla['Contenido_VA_Lista_Sigla'] == 'Seleccionar Sigla'
+        || databla['Contenido_VA_Lista_Motivo'] == 'Seleccionar Motivo') {
 
         DatoCompleto = 0;
 
     }
 
+    console.log(DatoCompleto);
+
     console.log(databla);
 
     if (DatoCompleto == 1) {
 
-        Swal.fire(
-            {
-                title: 'Mensaje del sistema',
-                text: "Usted esta registrando cambio de Sigla?",
-                icon: 'warning',
-                showCancelButton: true,
-                cancelButtonText: "Cancelar",
-                confirmButtonText: 'Si, Confirmar'
-            }).then((result) => {
+        if ($('#Contenido_VA_Sigla').val() != databla['Contenido_VA_Lista_Sigla']) {
 
-                if (result.isConfirmed) {
-                    $.ajax({
-                        url: ruta + 'Controlador_Funciones_Ajax',
-                        type: 'get',
-                        async: true,
-                        //data: { Controlador: 'Contenido_CH_Registrar', tabla: tabla },
-                        beforeSend: function (res) {
+            Swal.fire(
+                {
+                    title: 'Mensaje del sistema',
+                    text: "Usted esta registrando cambio de Sigla?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    cancelButtonText: "Cancelar",
+                    confirmButtonText: 'Si, Confirmar'
+                }).then((result) => {
 
-                            console.log("Cargando");
-                        },
-                        success: function (res) {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: ruta + 'Controlador_Funciones_Ajax',
+                            type: 'get',
+                            async: true,
+                            //data: { Controlador: 'Contenido_CH_Registrar', tabla: tabla },
+                            beforeSend: function (res) {
 
-                        }
-                    })
-                }
-            })
+                                console.log("Cargando");
+                            },
+                            success: function (res) {
+
+                            }
+                        })
+                    }
+                })
+        }else{
+            swal.fire('Mensaje del sistema', 'Validar la sigla Solicitada', 'warning')    
+        }
 
     }
     else {
